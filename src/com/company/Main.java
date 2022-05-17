89b5acd719a30b81a90b578e58f85c509e298136
noniyonikanory <nikanorypetro@icloud.com>
2022-05-17 20:43:42 +0300

    ASSIGNMENT 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double gravity = -9.81; // Earth's gravity in m/s^2
-        double initialVelocity = 0.0;
-        double fallingTime = 10.0;
-        double initialPosition = 0.0;
-        double finalPosition = 0.0;
-        System.out.println("The object's position after " + fallingTime +
-                " seconds is " + finalPosition + " m.");
+        double a = -9.81; // Earth's gravity in m/s^2
+        double Vi = 0.0;
+        double t= 10.0;
+        double xi= 0.0;
+        double xt;
+        xt=0.5*(a*t*t)+Vi*t + xi;
+        System.out.println("The object's position after " + t+
+                " seconds is " + xt + " m.");
     }
 }
